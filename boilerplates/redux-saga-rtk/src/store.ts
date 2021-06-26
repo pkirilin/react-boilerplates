fc3e6b90f43ce901ca011d06cce29687c1e6f94c
Pavel Kirilin <kirilin.pav@gmail.com>
2021-06-26 22:42:22 +0300

    Added redux-saga

--- boilerplates/redux-saga-rtk/src/store.ts
+++ boilerplates/redux-saga-rtk/src/store.ts
@@ -1,11 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import counterReducer from './features/counter/counterSlice';
+import rootSaga from './rootSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: () => [sagaMiddleware],
+  devTools: process.env.NODE_ENV !== 'production',
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
