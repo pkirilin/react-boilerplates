e5875cd4fe38c6e31da690a6d54adda309f079aa
Pavel Kirilin <kirilin.pav@gmail.com>
2021-07-24 17:28:07 +0300

    Removed redux-saga

--- boilerplates/rtk-query/src/app/root.saga.ts
+++
@@ -1,6 +0,0 @@
-import { all } from 'redux-saga/effects';
-import counterSaga from '../features/counter/counter.saga';
-
-export default function* rootSaga(): Generator {
-  yield all([counterSaga()]);
-}
--- boilerplates/rtk-query/src/test-utils/createTestStore.ts
+++ boilerplates/rtk-query/src/test-utils/createTestStore.ts
@@ -1,17 +1,12 @@
 import { AnyAction, applyMiddleware, createStore, Store } from '@reduxjs/toolkit';
-import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../app/root.reducer';
-import rootSaga from '../app/root.saga';
 import { StoreBuilderStep } from './test-utils.types';
 
 export default function createTestStore(...storeBuilderSteps: StoreBuilderStep[]): Store {
   const actions = getActions(...storeBuilderSteps);
   const initialState = actions.reduce(rootReducer, undefined);
-  const sagaMiddleware = createSagaMiddleware();
 
-  const store = createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware));
-
-  sagaMiddleware.run(rootSaga);
+  const store = createStore(rootReducer, initialState, applyMiddleware());
 
   return store;
 }
